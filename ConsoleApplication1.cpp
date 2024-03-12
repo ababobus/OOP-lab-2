26c7e9dfe9045c64233d3a83d81e93389f45fefd
Азалия Баймуратова <ozalea@yandex.ru>
2024-03-12 12:02:02 +0500

    first try

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,19 +2,108 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+
+using std::string;
+using std::ofstream;
+using std::stringstream;
+
+class SVG {
+private:
+	const string name;
+	ofstream f;
+public:
+	SVG(string name) : name(name) {
+		f = ofstream(name);
+		f << "<svg width=\"500\" height=\"500\""
+			<< " xmlns=\"http://www.w3.org/2000/svg\">\n";
+	}
+
+	void AddElement(string elem, string params) {
+		f << "<" << elem << " " << params << " />\n";
+	}
+
+	void Save() {
+		f << "</svg>";
+		f.close();
+	}
+
+};
+
+class Figure {
+private:
+	int x = 50, y = 50;
+	string stroke = "black";  string fill = "green";
+public:
+	virtual double Perimetr() = 0;
+	virtual double GetCenter() = 0;//центр
+	virtual double GetRightPoint() = 0; //середина правой стороны
+	virtual void AddTo(SVG& s) const = 0;
+
+
+	int GetX() const { return x; }
+	int GetY() const { return y; }
+
+	void SetX(int new_x) { x = new_x; }
+	void SetY(int new_y) { y = new_y; }
+
+	string GetFill() const { return fill; }
+	string GetStroke() const { return stroke; }
+
+	void SetFill(string color) { fill = color; }
+	void SetStroke(string color) { stroke = color; }
+};
+
+class Rectangle : public Figure {
+private:
+	int w = 70, h = 70;
+public:
+	Rectangle(int x, int y, int w, int h) {
+		SetX(x);
+		SetY(y);
+		this->w = w;
+		this->h = h;
+	}
+
+	void AddTo(SVG& s) const override {
+		stringstream p;
+		p << "x='" << GetX() - w / 2 << "' ";        p << "y='" << GetY() - h / 2 << "' ";
+		p << "width='" << w << "' ";        p << "height='" << h << "' ";
+		p << "stroke='" << GetStroke() << "' ";
+		p << "fill='" << GetFill() << "' ";
+		s.AddElement("rect", p.str());
+	}
+
+	double Perimetr() override {
+		return 2 * (w + h);
+	}
+	double GetCenter() override {
+		double centerX = (GetX() + w) / 2;
+		double centerY = (GetY() + h) / 2;
+		return centerX, centerY;
+	}
+	double GetRightPoint() override {
+		double rightX = w;
+		double rightY = (GetY() - h) / 2;
+		return rightX, rightY;
+	}
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	SVG s("picture1.svg");
+	Rectangle r(20, 0, 70, 70);
+	r.SetFill("pink");
+	r.AddTo(s);
+	std::cout<< r.Perimetr();
+
+
+	s.Save();
+	system("start picture1.svg");
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
